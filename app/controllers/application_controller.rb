4bcde2f6457fdbb3ec53790779e0705518ea5f02
Samuel Banya <sbanya@fastmail.com>
2022-09-17 21:23:34 -0500

    commit tree abe5cc0bcfbf4b4c7ce0a18d2adde7ef616dc0d5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,44 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  # POST /baked_goods: creates a new baked good in the database, 
+  # and returns data for the newly created baked good as JSON. 
+  # The request will receive an object like this as params:
+  post "/baked_goods" do 
+    # Related fields for 'BakedGood' class in 'db/migrate':
+      # t.string :name
+      # t.integer :price
+      # t.integer :bakery_id
+    baked_good = BakedGood.create(
+      # First attempt:
+      # :name params[:name], 
+      # :price params[:price], 
+      # :bakery_id params[:bakery_id])
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    baked_good.to_json()
+  end
+
+  # PATCH /bakeries/:id: updates the name of the bakery in the database, 
+  # and returns data for the updated bakery as JSON. 
+  # The request will receive an object like this as params:
+  patch "/bakeries/:id" do 
+    bakery = Bakery.find(params[:id])
+    # First attempt:
+    # bakery.name = params[:name]
+    bakery.update(
+      name: params[:name]
+    )
+    bakery.to_json()
+  end
+
+  # DELETE /baked_goods/:id: deletes the baked_good from the database.
+  delete "/baked_goods/:id" do 
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy()
+    baked_good.to_json()
+  end
+
 end
